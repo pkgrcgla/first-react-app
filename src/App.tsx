1fa4cea00cc8a4df7c6f0eb704d972e4a6320013
cagla <caglanurp@gmail.com>
2024-01-20 01:45:43 +0300

    text

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 function App() {
-  let items = ["New York", "San Francisco", "Izmir", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert text="Hello World" />
     </div>
   );
 }
